008aeddeca0d8c3f14fcf37bb32bf5ddad4609f1
yuewei shi <2671523019@qq.com>
2018-04-03 11:08:15 +0800

    完善首页

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,9 +15,62 @@
             <Dropdown-item divided><i class="fa fa-key"></i>Log out</Dropdown-item>
             <Dropdown-item divided><i class="fa fa-cog"></i>Setting</Dropdown-item>
           </Dropdown-menu>
-
         </Dropdown>
       </Row>
+      <div class="sidebar">
+         <Menu theme="dark"  width="100%" class="menu">
+           <Menu-item name="1">
+              <router-link to="/">
+                <i class="fa fa-dashboard"></i>
+                Dashboard
+              </router-link>
+           </Menu-item>
+           <Menu-item name="2">
+             <router-link to="/widget">
+               <i class="fa fa-cogs"></i>
+                Widget
+             </router-link>
+           </Menu-item>
+           <Menu-item name="3">
+             <router-link to="/charts">
+               <i class="fa fa-bar-chart"></i>
+               Charts
+             </router-link>
+           </Menu-item>
+           <Submenu name="4">
+              <template slot="title">
+                <i class="fa fa-database"></i>
+                Data Table
+              </template>
+              <Menu-item name="4-1">
+                 <router-link to="/basic-table">
+                    Basic Table
+                 </router-link>
+              </Menu-item>
+              <Menu-item name="4-2">
+               <router-link to="/editable-table">
+                 Editable Table
+               </router-link>
+             </Menu-item>
+           </Submenu>
+           <Submenu name="5">
+             <template slot="title">
+               <i class="fa fa-dashboard"></i>
+               Pages
+             </template>
+             <Menu-item name="5-1">
+               <router-link to="/login">
+                 Login
+               </router-link>
+             </Menu-item>
+             <Menu-item name="5-2">
+               <router-link to="/lockscreen">
+                 LockScreen
+               </router-link>
+             </Menu-item>
+           </Submenu>
+         </Menu>
+      </div>
    </div>
 </template>
 <script>
@@ -33,25 +86,25 @@
           msgPushData: [
             {
               image: require('@/assets/img/photo.jpg'),
-              from: 'JesseLuo',
-              time: '2017-01-08',
+              from: 'china',
+              time: '2018-03-08',
               message: 'I like your website very much'
             },
             {
               image: require('@/assets/img/photo.jpg'),
-              from: 'JesseLuo',
+              from: 'Japan',
               time: '2017-1-8',
               message: 'I like your website very much!'
             },
             {
               image: require('@/assets/img/photo.jpg'),
-              from: 'JesseLuo',
+              from: 'korean',
               time: '2017-1-8',
               message: 'I like your website very much!'
             },
             {
               image: require('@/assets/img/photo.jpg'),
-              from: 'JesseLuo',
+              from: 'USA',
               time: '2017-1-8',
               message: 'I like your website very much!'
             },
